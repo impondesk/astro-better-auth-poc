dc804c8077762eeaca480a748cabcd27755efeaf
Israni, Murli <murli.p.israni@gmail.com>
2026-01-12 12:37:54 +0530

    Add prerender flag to auth API route

--- src/pages/api/auth/[...auth].ts
+++ src/pages/api/auth/[...auth].ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { auth } from "../../../server/auth";
 
+export const prerender = false;
+
 export const ALL: APIRoute = async ({ request }) => {
   return auth.handler(request);
 };
